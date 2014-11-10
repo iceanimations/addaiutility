3dc0c809203f73d9b94a0645ba4c047a7b5eb488
Qurban Ali <qurban_ali36@yahoo.com>
2014-11-10 15:35:26 +0500

    meshes listed directly

--- src/_aiUtilityMaker.py
+++ src/_aiUtilityMaker.py
@@ -8,8 +8,8 @@ import pymel.core as pc
 import maya.cmds as mc
 
 def doTheMagic():
-    currentLayer = mc.editRenderLayerGlobals(currentRenderLayer=True, q=True)
-    objects = mc.editRenderLayerMembers(currentLayer, q=True, noRecurse=True)
+    currentLayer = pc.editRenderLayerGlobals(currentRenderLayer=True, q=True)
+    objects = pc.editRenderLayerMembers(currentLayer, q=True, noRecurse=True)
 #     if not objects:
 #         pc.warning("No objects found in the selected layer")
 #         return
@@ -17,34 +17,30 @@ def doTheMagic():
 #    newLayer = mc.createRenderLayer(objects, name='newLayer', noRecurse=True)
 #    mc.editRenderLayerGlobals(currentRenderLayer=newLayer)
 
-    meshes = set()
-    for obj in objects:
-        try:
-            meshes.add(mc.ls(obj, dag=True, type='mesh'))
-        except: pass
+    meshes = pc.ls(type='mesh')
     aiShaders = set()
     for mesh in meshes:
-        sgs = mc.listConnections(mesh, type='shadingEngine')
+        sgs = pc.listConnections(mesh, type='shadingEngine')
         if sgs:
             sgs = set(sgs)
             for sg in sgs:
                 try:
-                    ai = mc.listConnections(sg +'.surfaceShader')[0]
+                    ai = pc.listConnections(sg +'.surfaceShader')[0]
                 except: continue
             if type(pc.PyNode(ai)) == pc.nt.AiStandard:
                 aiShaders.add(ai)
     for aiSh in aiShaders:
         aiUtility = str(pc.Mel.eval('createRenderNodeCB -asShader "surfaceShader" aiUtility ""'))
-        utilsg = mc.listConnections(aiUtility, type='shadingEngine')[0]
+        utilsg = pc.listConnections(aiUtility, type='shadingEngine')[0]
         try:
-            mc.connectAttr(mc.listConnections(aiSh + '.color')[0] +'.outColor', aiUtility + '.color', f=True)
+            pc.connectAttr(pc.listConnections(aiSh + '.color')[0] +'.outColor', aiUtility + '.color', f=True)
         except IndexError:
-            mc.setAttr(aiUtility +'.color', mc.getAttr(aiSh +'.color'))
+            pc.setAttr(aiUtility +'.color', pc.getAttr(aiSh +'.color'))
         except: pass
-        mc.setAttr(aiUtility +'.shadeMode', 2)
-        for sg in mc.listConnections(aiSh, type='shadingEngine'):
+        pc.setAttr(aiUtility +'.shadeMode', 2)
+        for sg in pc.listConnections(aiSh, type='shadingEngine'):
             temp = []
-            members = mc.sets(sg, q=True)
+            members = pc.sets(sg, q=True)
             if members:
                 for mem in members:
                     try:
